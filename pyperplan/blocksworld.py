407aa62a0d4b154b176356c4b8208dc0d87daca6
refracc <s.anderson.23@abdn.ac.uk>
2024-06-19 15:17:41 +0100

    use pre-defined predicates

--- pyperplan/blocksworld.py
+++ pyperplan/blocksworld.py
@@ -18,83 +18,52 @@ print(holding)
 print(handempty)
 
 pick_up_effect = Effect(
-    addlist={Predicate("holding", [("?a", ["agent"]), ("?x", ["block"])])},
-    dellist={
-        Predicate("ontable", [("?x", ["block"])]),
-        Predicate("clear", [("?x", ["block"])]),
-        Predicate("handempty", [("?a", ["agent"])])
-    }
+    addlist={holding},
+    dellist={ontable, clear, handempty}
 )
 
 pick_up_action = Action(
     name="pick-up",
     signature=[("?a", ["agent"]), ("?x", ["block"])],
-    precondition=[
-        Predicate("clear", [("?x", ["block"])]),
-        Predicate("ontable", [("?x", ["block"])]),
-        Predicate("handempty", [("?a", ["agent"])])
-    ],
+    precondition=[clear, ontable, handempty],
     effect=pick_up_effect
 )
 
 put_down_effect = Effect(
-    addlist={
-        Predicate("clear", [("?x", ["block"])]),
-        Predicate("handempty", [("?a", ["agent"])]),
-        Predicate("ontable", [("?x", ["block"])])
-    },
-    dellist={Predicate("holding", [("?a", ["agent"]), ("?x", ["block"])])}
+    addlist={clear, handempty, ontable},
+    dellist={holding}
 )
 
 put_down_action = Action(
     name="put-down",
     signature=[("?a", ["agent"]), ("?x", ["block"])],
-    precondition=[Predicate("holding", [("?a", ["agent"]), ("?x", ["block"])])],
+    precondition=[holding],
     effect=put_down_effect
 )
 
 stack_effect = Effect(
-    addlist={
-        Predicate("clear", [("?x", ["block"])]),
-        Predicate("handempty", [("?a", ["agent"])]),
-        Predicate("on", [("?x", ["block"]), ("?y", ["block"])])
-    },
-    dellist={
-        Predicate("holding", [("?a", ["agent"]), ("?x", ["block"])]),
-        Predicate("clear", [("?y", ["block"])])
-    }
+    addlist={Predicate("clear", [("?x", ["block"])]), handempty,
+             Predicate("on", [("?x", ["block"]), ("?y", ["block"])])},
+    dellist={holding, Predicate("clear", [("?y", ["block"])])}
 )
 
 stack_action = Action(
     name="stack",
     signature=[("?a", ["agent"]), ("?x", ["block"]), ("?y", ["block"])],
-    precondition=[
-        Predicate("holding", [("?a", ["agent"]), ("?x", ["block"])]),
-        Predicate("clear", [("?y", ["block"])])
-    ],
+    precondition=[holding, Predicate("clear", [("?y", ["block"])])],
     effect=stack_effect
 )
 
 unstack_effect = Effect(
-    addlist={
-        Predicate("holding", [("?a", ["agent"]), ("?x", ["block"])]),
-        Predicate("clear", [("?y", ["block"])])
-    },
-    dellist={
-        Predicate("clear", [("?x", ["block"])]),
-        Predicate("handempty", [("?a", ["agent"])]),
-        Predicate("on", [("?x", ["block"]), ("?y", ["block"])])
-    }
+    addlist={holding, Predicate("clear", [("?y", ["block"])])},
+    dellist={Predicate("clear", [("?x", ["block"])]), handempty,
+             Predicate("on", [("?x", ["block"]), ("?y", ["block"])])}
 )
 
 unstack_action = Action(
     name="unstack",
     signature=[("?a", ["agent"]), ("?x", ["block"]), ("?y", ["block"])],
-    precondition=[
-        Predicate("on", [("?x", ["block"]), ("?y", ["block"])]),
-        Predicate("clear", [("?x", ["block"])]),
-        Predicate("handempty", [("?a", ["agent"])])
-    ],
+    precondition=[Predicate("on", [("?x", ["block"]), ("?y", ["block"])]), Predicate("clear", [("?x", ["block"])]),
+                  handempty],
     effect=unstack_effect
 )
-
